4afcc5bd8cd8c095a43c4312dc267fc18c1ea416
DESKTOP-PVEN00R\HP <v.ramon.montiel@sun.auto>
2026-03-08 17:05:45 -0600

    chore: init project structure

---
+++ liverpool-backend-challenge/src/main/java/com/liverpool/backend/LiverpoolBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.liverpool.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;application
+
+@SpringBootApplication
+public class LiverpoolBackendChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiverpoolBackendChallengeApplication.class, args);
+    }
+
+}
---
+++ liverpool-backend-challenge/src/test/java/com/liverpool/backend/LiverpoolBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liverpool.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiverpoolBackendChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
