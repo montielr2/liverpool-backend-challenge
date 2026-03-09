b2aeaed1a925570eb30b4e4487226ec8d9a4df10
DESKTOP-PVEN00R\HP <v.ramon.montiel@sun.auto>
2026-03-08 19:40:27 -0600

    fixing issue with Health rest controller & add mongoDB Conection to application.yaml

--- liverpool-backend-challenge/src/main/java/com/liverpool/backend/entrypoint/rest/HealthController.java
+++ liverpool-backend-challenge/src/main/java/com/liverpool/backend/entrypoint/rest/HealthController.java
@@ -2,10 +2,12 @@ package com.liverpool.backend.entrypoint.rest;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
+@RestController
 public class HealthController {
     @GetMapping("/health")
     public ResponseEntity<Map<String, Object>> health() {
